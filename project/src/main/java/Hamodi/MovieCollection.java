c26f62015b6c0522d30f154ee995b1bc7eae5c5d
HamodiBadljvi <94105961+HamodiBadljvi@users.noreply.github.com>
2022-02-12 15:34:08 +0100

    Added List of saved movies

---
+++ project/src/main/java/Hamodi/MovieCollection.java
@@ -0,0 +1,34 @@
+//package
+
+import java.util.ArrayList;
+
+public class MovieCollection {
+    
+    private ArrayList<Movie> movie = new ArrayList<Movie>();
+    private int nextPosition = 0;
+
+    public ArrayList<Movie> getMovie() {
+		return movie;
+	}
+	
+	public int getNumberOfMovie() {
+		return nextPosition;
+	}
+	
+	public void addMovie(Movie movie) {
+		movie.add = movie;
+		nextPosition++;
+	}
+	
+	public void findMovie(String title) throws MovieNotFoundException
+	{
+		title = title.trim();
+		
+		for (int counter = 0; counter < nextPosition; counter++) {
+			if (MovieArray[counter].getTitle().equalsIgnoreCase(title)) {
+				return MovieArray[counter];
+			}
+		}
+		throw new MovieNotFoundException();
+	}
+}
