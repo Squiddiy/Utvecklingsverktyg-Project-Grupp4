e0521c085b2dcceca2b0dc97c66d32880a019c36
HamodiBadljvi <94105961+HamodiBadljvi@users.noreply.github.com>
2022-02-12 15:35:06 +0100

    Added MovieTest

---
+++ project/src/test/java/HamodiTest/MovieTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+//package 
+
+public class MovieList {
+
+    private movieCollection mc;
+	private Movie movie;
+
+	public MovieCollection() {
+
+        @Test
+        public void testEquals() {
+            Movie movie1 = new movie(0,null,null,null,null,0);
+            Movie movie1 = new movie(0,null,null,null,null,0);
+            
+            assertTrue(book1.equals(book2));
+        }
+    
+        
+        @Test
+        public void testFindMovieIgnoringCase() {
+            
+            Game game1
+    
+        }
+    
+        @Test
+    	public void testNontEqualmovie() {
+        	Movie movie1 = new movie(0,null,null,null,null,0);
+        	Movie movie1 = new movie(0,null,null,null,null,0);
+    		
+    		assertFalse(movie1.equals(movie2));
+        
+}
