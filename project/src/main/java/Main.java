83b15c2593cad3cd2761932695ff208293c75862
urmandavid <davurm0211@skola.goteborg.se>
2022-02-12 14:14:22 +0100

    Adding Main class
    .

---
+++ project/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book(1, "The Fellowship of the Ring", "J.R.R Tolkien", "High Fantasy", "01234", 111);
+        Book book2 = new Book(2, "Dune", "Frank Herbert", "Science Fiction", "43210", 222);
+        Book book3 = new Book(3, "A Knight of the Seven Kingdoms", "George R.R. Martin", "Dark Fantasy", "56789", 333);
+        Book book4 = new Book(4, "The Blade Itself", "Joe Abercrombie", "Dark Fantasy", "98765", 444);
+    
+        ArrayList<Object> books = new ArrayList<Object>();
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+        books.add(book4);
+    }
+}
