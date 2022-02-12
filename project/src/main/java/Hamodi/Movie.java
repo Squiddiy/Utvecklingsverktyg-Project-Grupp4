21c8cad9dee18f3cd0e953fc3cea3c980da0e886
HamodiBadljvi <94105961+HamodiBadljvi@users.noreply.github.com>
2022-02-12 15:33:42 +0100

    Added basic movie format

---
+++ project/src/main/java/Hamodi/Movie.java
@@ -0,0 +1,49 @@
+//package 
+
+public class Movie {
+
+    private int id;
+    private String name;
+    private String director;
+    private double rating;
+
+    public Movie(int id, String name, String director, double rating) {
+        this.id=id;
+        this.name = name;
+        this.director = director;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
+
